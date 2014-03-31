8f7058c65ccdd13fe35d3fb089d0da1f5fa36b2d
António Garcrz <antonio_ribeiro01@hotmail.com>
2014-03-31 20:19:46 +0100

    Não levem a mal, é só mais este commit para dizer que acabei as referencias, que ficam de forma random

--- HealthPlusAPI/Models/ReferenceBank/Reference.cs
+++ HealthPlusAPI/Models/ReferenceBank/Reference.cs
@@ -9,17 +9,25 @@ namespace HealthPlusAPI.Models.ReferenceBank
     {
         public static List<Int32> NewReference()
         {
-            List<Int32> reference = new List<Int32>();
+            List<Int32> payment = new List<Int32>();
             
             // Referencia usada nos pagamentos multibanco
             // A referencia e um valor novo, gerado a cada pedido de uma nova referencia multibanco 
-            reference.Add(123456789);
+            Random rnd = new Random();
+            Int32 first_three = rnd.Next(0, 1000); // primeiros tres algarismos da referencia
+            Int32 second_three = rnd.Next(0, 1000); // segundos tres algarismos da referencia
+            Int32 third_three = rnd.Next(0, 1000); // ultimos tres algarismos da referencia
+
+            Int32 reference = (first_three * 1000000) + (second_three * 1000) + third_three; // formacao do valor da referencia
+            payment.Add(reference); // adicionar a referencia a lista
+ 
 
             // Entidade usada nos pagamentos multibanco: 99666
             // A entidade e um valor atribuido a empresa e que nao pode ser modificado
-            reference.Add(99666);
+            Int32 entity = 99666;
+            payment.Add(entity); // adicionar a entidade a lista
 
-            return reference;
+            return payment;
         }
     }
 }
