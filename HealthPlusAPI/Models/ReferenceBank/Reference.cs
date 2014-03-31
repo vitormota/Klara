ce79bfc9745609771f9f91e0562b99b44d3a72cd
António Garcrz <antonio_ribeiro01@hotmail.com>
2014-03-31 18:50:04 +0100

    Criação de uma classe para a geração de uma referencia multibanco (teste), e para ser testada alteração do controller values

---
+++ HealthPlusAPI/Models/ReferenceBank/Reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HealthPlusAPI.Models.ReferenceBank
+{
+    public class Reference
+    {
+        public static List<Int32> NewReference()
+        {
+            List<Int32> reference = new List<Int32>();
+            
+            reference.Add(123456789);
+            reference.Add(25693);
+
+            return reference;
+        }
+    }
+}
