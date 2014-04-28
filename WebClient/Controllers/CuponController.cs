8ebce90f2f06a35efee13de9071b67c56f872f2a
António Garcrz <antonio_ribeiro01@hotmail.com>
2014-04-28 12:16:45 +0100

    Geraçao do pdf pronto, e imagem do logo adicionada

--- WebClient/Controllers/CuponController.cs
+++ WebClient/Controllers/CuponController.cs
@@ -7,6 +7,8 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
 using System.Diagnostics;
+using System.Web.Hosting;
+using iTextSharp.text.pdf.draw;
 
 namespace WebClient_.Controllers
 {
@@ -39,6 +41,7 @@ namespace WebClient_.Controllers
             string path = Path.Combine(path_folder, filename);
             FileStream fs = new FileStream(path, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
             Document doc = new Document(PageSize.A4);
+            doc.SetMargins(iTextSharp.text.Utilities.MillimetersToPoints(30), iTextSharp.text.Utilities.MillimetersToPoints(30), iTextSharp.text.Utilities.MillimetersToPoints(25), iTextSharp.text.Utilities.MillimetersToPoints(25));
 
             // Criacao do ficheiro pdf
             PdfWriter writer = PdfWriter.GetInstance(doc, fs);
@@ -49,49 +52,57 @@ namespace WebClient_.Controllers
             BaseFont base_font_pdf = BaseFont.CreateFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
             Font font_pdf = new Font(base_font_pdf, 10);
 
-            // Desenhar linhas
-            float left = iTextSharp.text.Utilities.MillimetersToPoints((float)12.7);
-            float delta = (float)17.7;
+            // Criacao da linha que vai ser usada
+            Chunk line = new Chunk(new LineSeparator((float)1, 50, CMYKColor.BLACK, Element.ALIGN_LEFT, -1));
 
-            // Colocar linhas
-            for (int i = 0; i < 4; i++)
-            {
-                float top = iTextSharp.text.Utilities.MillimetersToPoints(269 - (i * delta));
+            // Colocar logo
+            Image health_plus_logo = Image.GetInstance(HostingEnvironment.MapPath(VirtualPathUtility.ToAbsolute("~/Content/Images/logo.png")));
+            health_plus_logo.ScalePercent(12);
+            doc.Add(health_plus_logo);
 
-                pdf_cb.MoveTo(left, top);
-                pdf_cb.LineTo(doc.PageSize.Width / 2, top);
-                pdf_cb.Stroke();
-            }
 
             // Colocacao do texto no pdf
             Paragraph p1 = new Paragraph("NOME", font_pdf);
             Paragraph p2 = new Paragraph(name, font_pdf);
-            p2.SpacingAfter = 20;
+            p1.SpacingBefore = 80;
             doc.Add(p1);
             doc.Add(p2);
 
+            // Desenho da linha 1
+            doc.Add(line);
+
             Paragraph p3 = new Paragraph("DESCRIÇÃO", font_pdf);
             Paragraph p4 = new Paragraph(description, font_pdf);
-            p4.SpacingAfter = 20;
+            p3.SpacingBefore = 10;
             doc.Add(p3);
             doc.Add(p4);
 
+            // Desenho da linha 2
+            doc.Add(line);
+
             Paragraph p5 = new Paragraph("INSTITUIÇÃO", font_pdf);
             Paragraph p6 = new Paragraph(institution, font_pdf);
-            p6.SpacingAfter = 20;
+            p5.SpacingBefore = 10;
             doc.Add(p5);
             doc.Add(p6);
+
+            // Desenho da linha 3
+            doc.Add(line);
 
             string date_str = date_cupon.Day.ToString() + "-" + date_cupon.Month.ToString() + "-" + date_cupon.Year.ToString() + " | " + date_cupon.Hour.ToString() + "h" + date_cupon.Minute.ToString();
             Paragraph p7 = new Paragraph("DATA", font_pdf);
             Paragraph p8 = new Paragraph(date_str, font_pdf);
-            p8.SpacingAfter = 20;
+            p7.SpacingBefore = 10;
             doc.Add(p7);
             doc.Add(p8);
+
+            // Desenho da linha 4
+            doc.Add(line);
 
             string price_str = price.ToString() + "€";
             Paragraph p9 = new Paragraph("PREÇO", font_pdf);
             Paragraph p10 = new Paragraph(price_str, font_pdf);
+            p9.SpacingBefore = 10;
             doc.Add(p9);
             doc.Add(p10);
             doc.Close();
