9bd34c9c5851067aece4ea49729b7d40cfba00bf
Pedro Alves <pedroalves0@gmail.com>
2014-05-13 12:40:19 +0100

    First version of institution template done

---
+++ WebInstitution/Controllers/DashboardController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebInstitution.Controllers
+{
+    public class DashboardController : Controller
+    {
+        //
+        // GET: /Dashboard/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Localization() {
+            return PartialView("_Localization");
+        }
+
+        public ActionResult Add() {
+            return PartialView("_Add");
+        }
+
+        public ActionResult Settings() {
+            return PartialView("_Settings");
+        }
+
+        public ActionResult Edit() {
+            return PartialView("_Edit");
+        }
+
+        public ActionResult Stats() {
+            return PartialView("_Stats");
+        }
+	}
+}
