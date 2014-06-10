745d68e5c1d8f50c39bffb5d999a09c1f4d08e1c
Ricardo Teixeira <ricardofmteixeria@gmail.com>
2014-06-10 10:26:08 +0100

    Multi Language started on WebClient

--- WebClient/Controllers/HomeController.cs
+++ WebClient/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Web;
 using System.Web.Mvc;
 using WebClient_.HealthPService;
 using WebClient_.Models;
+using WebInstitution.Helpers;
 
 namespace WebClient_.Controllers
 {
@@ -31,5 +34,23 @@ namespace WebClient_.Controllers
 
             return PartialView("_AdResult", ad);
         }
+
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }   
     }
 }
